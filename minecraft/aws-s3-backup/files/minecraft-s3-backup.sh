74bb4077fed1ed5927e4e0385c75d469af6debbf
Luke Hoersten <luke@hoersten.org>
2021-07-03 13:38:02 -0500

    Updated minecraft backup for lifecycles.

--- minecraft/aws-s3-backup/files/minecraft-s3-backup.sh
+++ minecraft/aws-s3-backup/files/minecraft-s3-backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 BUCKET=$1
-DATE=`date --iso-8601`
 BACKUP_DIR=$2
-BACKUP_TAR="/tmp/$BUCKET-$DATE.tgz"
+BACKUP_TAR="/tmp/$BUCKET.tgz"
 
 tar -zc -f $BACKUP_TAR $BACKUP_DIR
 aws s3 mb "s3://$BUCKET/"
+aws s3api put-bucket-versioning --bucket "$BUCKET" --versioning-configuration Status=Enabled
+aws s3api put-bucket-lifecycle-configuration --bucket "$BUCKET" --lifecycle-configuration "file:///usr/local/share/minecraft-s3-backup-lifecycle.json"
 aws s3 cp $BACKUP_TAR "s3://$BUCKET/"
 
 rm $BACKUP_TAR
