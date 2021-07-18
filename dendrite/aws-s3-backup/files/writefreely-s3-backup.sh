9bb76f57d55ca5a9e502a34e504a21904d4157a1
Luke Hoersten <luke@hoersten.org>
2021-07-18 17:43:46 -0500

    Tons of updates.

---
+++ dendrite/aws-s3-backup/files/writefreely-s3-backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BUCKET=$1
+BACKUP_DIR=$2
+BACKUP_TAR="/tmp/$BUCKET.tgz"
+
+tar -zc -f $BACKUP_TAR $BACKUP_DIR
+aws s3 mb "s3://$BUCKET/"
+aws s3api put-bucket-versioning --bucket "$BUCKET" --versioning-configuration Status=Enabled
+aws s3api put-bucket-lifecycle-configuration --bucket "$BUCKET" --lifecycle-configuration "file:///usr/local/share/writefreely-s3-backup-lifecycle.json"
+aws s3 cp $BACKUP_TAR "s3://$BUCKET/"
+
+rm $BACKUP_TAR
