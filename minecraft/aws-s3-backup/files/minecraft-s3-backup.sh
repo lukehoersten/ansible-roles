769d10b216d52482eca1de9ca9491c3ab8fc3f5f
Luke Hoersten <luke@hoersten.org>
2021-07-18 20:17:16 -0500

    Updated minecraft to be multi-world and minimized world size.

--- minecraft/aws-s3-backup/files/minecraft-s3-backup.sh
+++ minecraft/aws-s3-backup/files/minecraft-s3-backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-BUCKET=$1
+BACKUP_WORLD_NAME=$1
 BACKUP_DIR=$2
-BACKUP_TAR="/tmp/$BUCKET.tgz"
+BUCKET=$3
+BACKUP_TAR_DIR=$4
+BACKUP_TAR="${BACKUP_TAR_DIR}/${BUCKET}.tgz"
 
-tar -zc -f $BACKUP_TAR $BACKUP_DIR
+tar -zc -f $BACKUP_TAR -C $BACKUP_DIR $BACKUP_WORLD_NAME {$BACKUP_WORLD_NAME}_nether ${BACKUP_WORLD_NAME}_the_end
 aws s3 mb "s3://$BUCKET/"
 aws s3api put-bucket-versioning --bucket "$BUCKET" --versioning-configuration Status=Enabled
 aws s3api put-bucket-lifecycle-configuration --bucket "$BUCKET" --lifecycle-configuration "file:///usr/local/share/minecraft-s3-backup-lifecycle.json"
