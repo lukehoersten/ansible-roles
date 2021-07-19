b8a91813ec86478143d2bca75f8bc937fab274e5
Luke Hoersten <luke@hoersten.org>
2021-07-18 20:26:54 -0500

    Fixed backup script error.

--- minecraft/aws-s3-backup/files/minecraft-s3-backup.sh
+++ minecraft/aws-s3-backup/files/minecraft-s3-backup.sh
@@ -6,7 +6,7 @@ BUCKET=$3
 BACKUP_TAR_DIR=$4
 BACKUP_TAR="${BACKUP_TAR_DIR}/${BUCKET}.tgz"
 
-tar -zc -f $BACKUP_TAR -C $BACKUP_DIR $BACKUP_WORLD_NAME {$BACKUP_WORLD_NAME}_nether ${BACKUP_WORLD_NAME}_the_end
+tar -zc -f $BACKUP_TAR -C $BACKUP_DIR "$BACKUP_WORLD_NAME" "${BACKUP_WORLD_NAME}_nether" "${BACKUP_WORLD_NAME}_the_end"
 aws s3 mb "s3://$BUCKET/"
 aws s3api put-bucket-versioning --bucket "$BUCKET" --versioning-configuration Status=Enabled
 aws s3api put-bucket-lifecycle-configuration --bucket "$BUCKET" --lifecycle-configuration "file:///usr/local/share/minecraft-s3-backup-lifecycle.json"
