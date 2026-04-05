e925453eab7024b5f169bbeef6a281c9952b1d30
Luke Hoersten <luke@hoersten.org>
2026-04-05 15:50:31 -0500

    Add git/aws-s3-backup and git/web (cgit) roles

---
+++ git/aws-s3-backup/files/git-s3-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BUCKET=$1
+BACKUP_DIR=$2
+DATE=$(date +%Y-%m-%d)
+BACKUP_TAR=$(mktemp /tmp/git-s3-backup.XXXXXX.tgz)
+
+tar -zc -f "$BACKUP_TAR" "$BACKUP_DIR"
+aws s3 mb "s3://$BUCKET/"
+aws s3api put-bucket-versioning --bucket "$BUCKET" --versioning-configuration Status=Enabled
+aws s3api put-bucket-lifecycle-configuration --bucket "$BUCKET" --lifecycle-configuration "file:///usr/local/share/git-s3-backup-lifecycle.json"
+aws s3 cp "$BACKUP_TAR" "s3://$BUCKET/git-s3-backup-$DATE.tgz"
+
+rm "$BACKUP_TAR"
--- mercurial/aws-s3-backup/files/mercurial-s3-backup.sh
+++ mercurial/aws-s3-backup/files/mercurial-s3-backup.sh
@@ -2,12 +2,13 @@
 
 BUCKET=$1
 BACKUP_DIR=$2
-BACKUP_TAR="/tmp/$BUCKET.tgz"
+DATE=$(date +%Y-%m-%d)
+BACKUP_TAR=$(mktemp /tmp/mercurial-s3-backup.XXXXXX.tgz)
 
-tar -zc -f $BACKUP_TAR $BACKUP_DIR
+tar -zc -f "$BACKUP_TAR" "$BACKUP_DIR"
 aws s3 mb "s3://$BUCKET/"
 aws s3api put-bucket-versioning --bucket "$BUCKET" --versioning-configuration Status=Enabled
 aws s3api put-bucket-lifecycle-configuration --bucket "$BUCKET" --lifecycle-configuration "file:///usr/local/share/mercurial-s3-backup-lifecycle.json"
-aws s3 cp $BACKUP_TAR "s3://$BUCKET/"
+aws s3 cp "$BACKUP_TAR" "s3://$BUCKET/mercurial-s3-backup-$DATE.tgz"
 
-rm $BACKUP_TAR
+rm "$BACKUP_TAR"
